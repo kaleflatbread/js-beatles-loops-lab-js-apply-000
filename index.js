3d8a206842879dd531213654f5a878eab54882da
Kayla Fitzgerald <fitzgerald.km11@gmail.com>
2018-04-14 16:18:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ function iLoveTheBeatles(num){
       do{
         finalAnswer.push("I love the Beatles!")
          num++;
-      } while (i<num);
+      } while (num<15);
     return finalAnswer;
   }
 }
